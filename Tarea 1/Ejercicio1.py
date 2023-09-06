235eef39479d4850c0d5d80a288deafff23fe743
DanRoal <115745806+DanRoal@users.noreply.github.com>
2023-09-06 07:45:58 -0600

    Primer ejercicio

---
+++ Tarea 1/Ejercicio1.py
@@ -0,0 +1,8 @@
+"""
+Escribir un programa que pida al usuario una palabra y la muestre en pantalla 10 veces
+"""
+
+palabra = input("Por favor introduce una palabra para repetirla:")
+
+for i in range(10):
+    print(palabra)
