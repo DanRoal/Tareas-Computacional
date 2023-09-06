d3a826353e84f39abfaebd013db089d578602b9b
DanRoal <115745806+DanRoal@users.noreply.github.com>
2023-09-06 14:46:02 +0000

    Correccion ejercicio 3 y ejercicio 4

--- Tarea 1/Ejercicio3.py
+++ Tarea 1/Ejercicio3.py
@@ -1,9 +1,12 @@
 """
 Un programa que pide al usuario un entero positivo y muestre 
-por pantalla la cuenta atras desde ese numero hasta 0
+por pantalla la cuenta atras desde ese numero hasta 0 separados por comas
 """
 
 numero = int(input("Por favor introduza numero para cuenta regresiva:"))
+lista = []
 
-for i in range(-numero,1):
-    print(-1*i)
+for i in range(-numero,1): 
+    lista.append(-1*i)
+
+print(lista)
---
+++ Tarea 1/Ejercicio4.py
@@ -0,0 +1,11 @@
+"""
+Programa que pregunte al usuario una cantidad a invertir, el interes anual y el numero
+de años, y muestre en pantalla el capital obtenido en la inversión cada año que dura
+"""
+
+inversionInicial = float(input("Introduzca su inversion inicial"))
+interes = float(input("introduzca la tasa de interes como porcentaje:"))
+anos = int(input("Por favor introduzca cuantos años va a dejar su dinero"))
+
+for i in range(1,anos+1):
+    print((inversionInicial + (interes)/100)**i)
