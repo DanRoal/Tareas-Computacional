d4ed26ebf867132614c5d17843cb1ea4b451607f
DanRoal <115745806+DanRoal@users.noreply.github.com>
2023-09-14 05:20:22 +0000

    ej 2 tarea 2

---
+++ Tarea 2/Ejercicio 1.py
@@ -0,0 +1,14 @@
+"""
+Escribir un programa que almacene en una lista los numeros del 1 al 10 y los
+muestre en orden inverso separados por comas
+"""
+lista = []
+for i in range(1,11):
+    lista.append(i)
+cuenta= ''
+for i in range(1,11):
+    if lista[-i]==lista[0]: 
+        cuenta = cuenta + f"{lista[0]}"
+    cuenta = cuenta + f"{lista[-i]}, "
+
+print(cuenta)
