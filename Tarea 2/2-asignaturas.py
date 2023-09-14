212ce7d7ec424e08819ce072322ccfed649a0c5f
Dan <danroal@ciencias.unam.mx>
2023-09-14 00:54:22 -0600

    Cambios de nombres
    Los nombres de tarea 2 ahora son descriptivos

---
+++ Tarea 2/2-asignaturas.py
@@ -0,0 +1,5 @@
+"""
+Escribir un programa que almacene las asignaturas de un curso (Por ejemplo
+matemáticas, física) en una lista y la muestre por pantalla el mensaje
+'Yo estudio <asignatura> donde <asignatura> es cada una de las asignaturas de la lista
+"""
---
+++ Tarea 2/3-reprobadas.py
@@ -0,0 +1,7 @@
+"""
+Escribir un programa que almacene las asignaturas de  un curso, (Por ejemplo
+Matetmáticas, física, química historia y lengua) en una lista, pregunte
+al usuario la nota que ha sacado en cada asignatura y elimine de la lista
+las asignaturas aprobadas. al final el progrma debe mostrar porpantalla las
+asignaturas que el usuario debe repetir.
+"""
