6195253a8aa1d9282e6f34bbbeab978a1cae4f64
DanRoal <115745806+DanRoal@users.noreply.github.com>
2023-09-14 19:29:36 +0000

    ejercicio 2 y 3 tarea 2

--- Tarea 2/2-asignaturas.py
+++ Tarea 2/2-asignaturas.py
@@ -3,3 +3,13 @@ Escribir un programa que almacene las asignaturas de un curso (Por ejemplo
 matemáticas, física) en una lista y la muestre por pantalla el mensaje
 'Yo estudio <asignatura> donde <asignatura> es cada una de las asignaturas de la lista
 """
+
+asignaturas= ['Fisica atomica y materia condensada',
+            'Fisica computacional',
+            'Mecanica cuantica',
+            'fisica estadistica',
+            'Laboratorio de Electronica',
+            'Electromagnetismo 2']
+
+for asignatura in asignaturas:
+    print(f'Yo estudio {asignatura}')
--- Tarea 2/3-reprobadas.py
+++ Tarea 2/3-reprobadas.py
@@ -5,3 +5,16 @@ al usuario la nota que ha sacado en cada asignatura y elimine de la lista
 las asignaturas aprobadas. al final el progrma debe mostrar porpantalla las
 asignaturas que el usuario debe repetir.
 """
+
+materias = ['espanol',
+            'matematicas',
+            'fisica',
+            'quimica']
+
+reprobadas = []
+for asignatura in materias:
+    calif = int(input(f'cual fue tu calificacion en la asignatura de {asignatura}:'))
+    if calif < 6:
+        reprobadas.append(asignatura)
+
+print('Las materias que reprobaste fueron: ' + ', '.join(i for i in reprobadas))
