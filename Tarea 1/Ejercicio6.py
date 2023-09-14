d14953f9978e88caeac0a2521c6637bdbb941c64
DanRoal <115745806+DanRoal@users.noreply.github.com>
2023-09-14 00:59:25 +0000

    pulido ejercicio 6

--- Tarea 1/Ejercicio6.py
+++ Tarea 1/Ejercicio6.py
@@ -6,12 +6,15 @@ import math as m
 numero = int(input("Introduzca un número para saber si es primo o no:"))
 
 if numero >= 2:
-    for i in range(2,m.floor(m.sqrt(numero))+1):
+    for i in range(2,m.floor(m.sqrt(numero))+2):
         modulo= numero%i
-        if modulo!= 0: continue
+        if ( modulo!= 0) and (i<m.floor(m.sqrt(numero))+1 ): continue
+        elif i == m.floor(m.sqrt(numero))+1:
+            print(f'El número {numero} es primo yeii')
         else: print(f"El numero {numero} es divisible por {i}, por lo que no es primo"); break
-        print(f'El número {numero} es primo yeii')
+
 elif numero == 1:
     print("El número 1 no es primo")
 elif numero <=1:
-    print("Introduce un positivo, vuelve a intentarlo")
+    print("Introduce un positivo, vuelve a intentarlo\n")
+    
