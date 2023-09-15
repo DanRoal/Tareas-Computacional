66f5d491f2c22796837b424528fa28b0e0fba0c8
Dan <danroal@ciencias.unam.mx>
2023-09-15 00:28:05 -0600

    Contar vocales por medio de un diccionario

--- Tarea 2/6-vocales.py
+++ Tarea 2/6-vocales.py
@@ -1,4 +1,16 @@
 """
-escribir un programa que pida al usuario una frase u oración,
+Escribir un programa que pida al usuario una frase u oración,
 y muestre por pantalla el número de veces que contiene cada vocal
 """
+
+frase = list(input("Por favor introduca una frase para contar las vocales:\n"))
+
+dicc ={
+    'a': frase.count("a") + frase.count("A"),
+    'e': frase.count("e") + frase.count("E"),
+    'i': frase.count("i") + frase.count("I"),
+    'o': frase.count("o") + frase.count("O"),
+    'u': frase.count("u") + frase.count("U")
+}
+
+print(dicc)
