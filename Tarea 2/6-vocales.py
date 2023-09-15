076aaa022c4debae56c9fca879d86fd7bd8b8192
Dan <danroal@ciencias.unam.mx>
2023-09-14 19:35:37 -0600

    instrucciones ejercicios 6,7,8,9,10

---
+++ Tarea 2/6-vocales.py
@@ -0,0 +1,4 @@
+"""
+escribir un programa que pida al usuario una frase u oración,
+y muestre por pantalla el número de veces que contiene cada vocal
+"""
---
+++ Tarea 2/9-desviacionEstandar.py
@@ -0,0 +1,5 @@
+"""
+Escribir un programa que pregunte por una muestra de números
+separados por comas, los guarde en una lista y muestre por pantalla
+su media y desviación estandar
+"""
