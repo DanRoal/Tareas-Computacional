0642272b682a73cbd8233a045f6a7d3aa756616d
Dan <danroal@ciencias.unam.mx>
2023-09-15 21:25:20 -0600

    Producto matricial con ciclos for

---
+++ Tarea 2/10-productoMatrices.py
@@ -0,0 +1,19 @@
+"""
+Escribe un programa que almacene las 2 matrices A y B en listas y muestre en pantalla su producto
+"""
+
+def productMat(MatrizA, MatrizB):
+    resultado = []
+    for i in range(len(MatrizA)):
+        resultado.append([])
+        fila_i = 0
+        for h in range(len(MatrizB[i])):
+            for x in range(len(MatrizA[i])):
+                fila_i += MatrizA[i][x] * MatrizB[x][h]
+            resultado[i].append(fila_i)
+    return resultado
+
+A = [[1,2,3], [4,5,6]]
+B = [[1,-1],[0,1],[-1,1]]
+
+print(productMat(A,B))
