486e9333b433f7abf7e279cc952c5bbf767898d1
DanRoal <115745806+DanRoal@users.noreply.github.com>
2023-10-18 15:42:18 +0000

    generacion de numeros aleatorios

---
+++ Tarea 5/Generador_Numeros_Aleatorios.py
@@ -0,0 +1,9 @@
+"""
+Escribe un programa que genere y muestre 20 numeros aleatorios extraidos de una 
+distribucion uniforme entre -5 y 10
+"""
+
+import numpy as np
+
+print(np.random.uniform(-5,10,20))
+      
